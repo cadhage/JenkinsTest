0891f1208bdb594a0e4c67046bfbfcb87559bfd3
cadhage <cadhage@gmail.com>
2023-10-12 03:00:51 +0530

    Adding maven test project for jenkins

---
+++ src/main/java/com/jenkins/practice/jenkins/Controller/JenkinsTestController.java
@@ -0,0 +1,12 @@
+package com.jenkins.practice.jenkins.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JenkinsTestController {
+    @GetMapping("/")
+    public String helloWorld(){
+        return "Hello World";
+    }
+}
